742fbb181f9814772c4875a644773f1f09bf5705
Tomasz Kowal <tomasz.kowal@clubcollect.com>
2020-02-17 10:02:29 +0100

    Setup CI caches

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule ComposableHtml.MixProject do
       version: "0.1.0",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      dialyzer: dialyzer(Mix.env())
     ]
   end
 
@@ -18,13 +19,23 @@ defmodule ComposableHtml.MixProject do
     ]
   end
 
+  # Setup dialyzer paths for CI Cache
+  defp dialyzer(:test) do
+    [
+      plt_core_path: "priv/plts",
+      plt_file: {:no_warn, "priv/plts/dialyzer.plt"}
+    ]
+  end
+
+  defp dialyzer(_), do: []
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:phoenix_html, "~> 2.11"},
-      {:dialyxir, "~> 0.5", only: [:dev], runtime: false}
+      {:dialyxir, "~> 0.5", only: [:dev, :test], runtime: false}
     ]
   end
 end
